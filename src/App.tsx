dec15fcb576f2ba7d7c7f595853a954548565c52
Alba Vega <332845544+albavega-dev@users.noreply.github.com>
2026-09-24 20:16:55 +0200

    refactor: organize app imports and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
-import About from './pages/about/About'
-import Home from './pages/home/Home'
-import Navbar from './components/layout/Navbar'
+
 import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+
+import About from './pages/about/About'
 import Contact from './pages/contact/Contact'
+import CV from './pages/cv/CV'
+import Experience from './pages/experience/Experience'
+import Home from './pages/home/Home'
 import Projects from './pages/projects/Projects'
 import UILab from './pages/ui-lab/UILab'
-import Experience from './pages/experience/Experience'
-import CV from './pages/cv/CV'
 
 function App() {
   return (
@@ -17,11 +19,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/experience" element={<Experience />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/ui-lab" element={<UILab />} />
-        <Route path="/experience" element={<Experience />} />
         <Route path="/cv" element={<CV />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
 
       <Footer />
