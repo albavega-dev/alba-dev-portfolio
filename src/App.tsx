3a2dfa1fa882a1dc24d5dd3884fe9d9c0f5a7ec1
Alba Vega <332845544+albavega-dev@users.noreply.github.com>
2026-09-24 19:51:24 +0200

    refactor: add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import About from './pages/about/About'
 import Home from './pages/home/Home'
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="p-6 text-center">
+      <p>© 2026 Alba Vega Dev</p>
+    </footer>
+  )
+}
+
+export default Footer
