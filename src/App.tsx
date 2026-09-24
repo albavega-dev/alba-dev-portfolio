86c432f9bf070f02a4dd406c0214ec9bf24f5bb5
Alba Vega <332845544+albavega-dev@users.noreply.github.com>
2026-09-24 20:05:14 +0200

    feat: add projects page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/home/Home'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Contact from './pages/contact/Contact'
+import Projects from './pages/projects/Projects'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
 
       <Footer />
---
+++ src/pages/projects/Projects.tsx
@@ -0,0 +1,12 @@
+function Projects() {
+  return (
+    <main className="min-h-screen p-8">
+      <h1 className="text-4xl font-bold">Projects</h1>
+      <p className="mt-4 text-lg">
+        A selection of my personal and professional projects.
+      </p>
+    </main>
+  )
+}
+
+export default Projects
